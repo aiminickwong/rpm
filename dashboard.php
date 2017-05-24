d1dc2fa5836b18ba2a5a42b71103ce09de5b33cf
Seitanas <tadas@ring.lt>
2017-05-24 15:09:28 +0300

    Updating file structure

--- dashboard.php
+++ dashboard.php
@@ -18,48 +18,34 @@ set_lang();
 <html>
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="Tadas Ustinavičius">
-
     <title><?php echo _("Remote Power Management - dashboard");?></title>
-
     <!-- Bootstrap Core CSS -->
-    <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-
+    <link href="inc/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- MetisMenu CSS -->
-    <link href="bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
-
+    <link href="inc/bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
     <!-- DataTables CSS -->
-    <link href="bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.css" rel="stylesheet">
-
+    <link href="inc/bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.css" rel="stylesheet">
     <!-- DataTables Responsive CSS -->
-    <link href="bower_components/datatables-responsive/css/responsive.dataTables.scss" rel="stylesheet">
-
+    <link href="inc/bower_components/datatables-responsive/css/responsive.dataTables.scss" rel="stylesheet">
     <!-- Timeline CSS -->
-    <link href="dist/css/timeline.css" rel="stylesheet">
-
+    <link href="inc/dist/css/timeline.css" rel="stylesheet">
     <!-- Custom CSS -->
-    <link href="dist/css/sb-admin-2.css" rel="stylesheet">
-
+    <link href="inc/dist/css/sb-admin-2.css" rel="stylesheet">
     <!-- Custom RPM CSS -->
-    <link href="css/rpm.css" rel="stylesheet">
-
+    <link href="inc/css/rpm.css" rel="stylesheet">
     <!-- Custom Fonts -->
-    <link href="bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-	
-
-
+    <link href="inc/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesnt work if you view the page via file:// -->
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-
 </head>
 
 <body>
@@ -339,26 +325,19 @@ set_lang();
 
     </div>
     <!-- /#wrapper -->
-
     <!-- jQuery -->
-    <script src="bower_components/jquery/dist/jquery.min.js"></script>
-
+    <script src="inc/bower_components/jquery/dist/jquery.min.js"></script>
     <!-- Bootstrap Core JavaScript -->
-    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-
+    <script src="inc/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
     <!-- Metis Menu Plugin JavaScript -->
-    <script src="bower_components/metisMenu/dist/metisMenu.min.js"></script>
-
-    <script src="js/multiselect.js"></script>
-
+    <script src="inc/bower_components/metisMenu/dist/metisMenu.min.js"></script>
+    <script src="inc/js/multiselect.js"></script>
     <!-- DataTables JavaScript -->
-    <script src="bower_components/datatables/media/js/jquery.dataTables.min.js"></script>
-    <script src="bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.min.js"></script>
-    <script src="bower_components/datatables-responsive/js/dataTables.responsive.js"></script>
-
-
+    <script src="inc/bower_components/datatables/media/js/jquery.dataTables.min.js"></script>
+    <script src="inc/bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.min.js"></script>
+    <script src="inc/bower_components/datatables-responsive/js/dataTables.responsive.js"></script>
     <!-- Custom Theme JavaScript -->
-    <script src="dist/js/sb-admin-2.js"></script>
+    <script src="inc/dist/js/sb-admin-2.js"></script>
 
 <script>
 function redraw_info_panels(){
